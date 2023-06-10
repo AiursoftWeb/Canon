72e75c85f2a6b92aa3bd6df0c28e3fc22bbe3330
Anduin Xue <anduin@aiursoft.com>
2023-06-10 18:47:52 +0000

    Migrate code fore retry engine and cache service.

---
+++ src/Aiursoft.Canon/CacheService.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+
+namespace Aiursoft.Canon;
+
+public class CacheService
+{
+    private readonly IMemoryCache _cache;
+    private readonly ILogger<CacheService> _logger;
+
+    public CacheService(
+        IMemoryCache cache,
+        ILogger<CacheService> logger)
+    {
+        _cache = cache;
+        _logger = logger;
+    }
+
+    public async Task<T?> RunWithCache<T>(
+        string cacheKey,
+        Func<Task<T>> fallback,
+        Predicate<T>? cacheCondition = null,
+        int cachedMinutes = 20)
+    {
+        cacheCondition ??= (_) => true;
+
+        if (!this._cache.TryGetValue(cacheKey, out T resultValue) || resultValue == null || cachedMinutes <= 0 ||
+            cacheCondition(resultValue) == false)
+        {
+            resultValue = await fallback();
+            if (resultValue == null)
+            {
+                return default;
+            }
+            else if (cachedMinutes > 0 && cacheCondition(resultValue))
+            {
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(TimeSpan.FromMinutes(cachedMinutes));
+
+                this._cache.Set(cacheKey, resultValue, cacheEntryOptions);
+                this._logger.LogInformation("Cache set for {CachedMinutes} minutes with cached key: {CacheKey}", cachedMinutes, cacheKey);
+            }
+        }
+        else
+        {
+            this._logger.LogInformation("Cache was hit with cached key: {CacheKey}", cacheKey);
+        }
+
+        return resultValue;
+    }
+
+    public async Task<T2?> QueryCacheWithSelector<T1, T2>(
+        string cacheKey,
+        Func<Task<T1>> fallback,
+        Func<T1, T2> selector,
+        Predicate<T1>? cacheCondition = null,
+        int cachedMinutes = 20)
+    {
+        cacheCondition ??= (_) => true;
+
+        if (!this._cache.TryGetValue(cacheKey, out T1 resultValue) || resultValue == null || cachedMinutes <= 0 ||
+            cacheCondition(resultValue) == false)
+        {
+            resultValue = await fallback();
+            if (resultValue == null)
+            {
+                return default;
+            }
+
+            if (cachedMinutes > 0 && cacheCondition(resultValue))
+            {
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(cachedMinutes));
+
+                this._cache.Set(cacheKey, resultValue, cacheEntryOptions);
+                this._logger.LogInformation("Cache set for {CachedMinutes} minutes with cached key: {CacheKey}", cachedMinutes, cacheKey);
+            }               
+
+        }
+        else
+        {
+            this._logger.LogInformation("Cache was hit with cached key: {CacheKey}", cacheKey);
+        }
+
+        return selector(resultValue);
+    }
+
+    public void Clear(string key)
+    {
+        this._cache.Remove(key);
+    }
+}
---
+++ src/Aiursoft.Canon/RetryEngine.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Aiursoft.Canon;
+
+public class RetryEngine
+{
+    private static readonly Random Rnd = new();
+    private readonly ILogger<RetryEngine> _logger;
+
+    /// <summary>
+    ///     Creates new retry engine.
+    /// </summary>
+    /// <param name="logger">Logger</param>
+    public RetryEngine(ILogger<RetryEngine> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<T> RunWithRetry<T>(
+        Func<int, Task<T>> taskFactory,
+        int attempts = 3,
+        Predicate<Exception>? when = null)
+    {
+        for (var i = 1; i <= attempts; i++)
+        {
+            try
+            {
+                _logger.LogDebug("Starting a job with retry... Attempt: {Attempt} (Starts from 1)", i);
+                var response = await taskFactory(i);
+                return response;
+            }
+            catch (Exception e)
+            {
+                if (when != null)
+                {
+                    var shouldRetry = when.Invoke(e);
+                    if (!shouldRetry)
+                    {
+                        _logger.LogWarning(e, $"A task that was asked to retry failed. But from the given condition is false, we gave up retry.");
+                        throw;
+                    }
+                }
+
+                if (i >= attempts)
+                {
+                    _logger.LogWarning(e,
+                        "A task that was asked to retry failed. Maximum attempts {Max} already reached. We have to crash it", attempts);
+                    throw;
+                }
+
+                _logger.LogCritical(e,
+                    "A task that was asked to retry failed. Will retry soon. Current attempt is {Current}. maximum attempts is {Max}", i, attempts);
+
+                await Task.Delay(ExponentialBackoffTimeSlot(i) * 1000);
+            }
+        }
+
+        throw new InvalidOperationException("Code shall not reach here.");
+    }
+
+    private static int ExponentialBackoffTimeSlot(int time)
+    {
+        var max = (int)Math.Pow(2, time);
+        return Rnd.Next(0, max);
+    }
+}
